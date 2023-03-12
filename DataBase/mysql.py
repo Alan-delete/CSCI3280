a1e4a79010ff202541dd8d48668a1d6ca1fd5ad5
Alan-delete <62469498+Alan-delete@users.noreply.github.com>
2023-03-12 16:10:08 +0800

    Add search function

--- DataBase/mysql.py
+++ DataBase/mysql.py
@@ -4,58 +4,100 @@ HOST = 'localhost'
 USER = 'csci3280'
 PASSWORD = 'csci3280'
 DATABASE = 'project'
+ATTRIBUTES = {'id': 'INT AUTO_INCREMENT PRIMARY KEY',
+              'name': 'CHAR(40) NOT NULL',
+              'time': 'TIME NOT NULL',
+              'author': 'CHAR(40)',
+              'album' : 'CHAR(40)'}
+
+class my_Database():
+    def __init__(self, host = HOST,user= USER, password= PASSWORD, database= DATABASE ) -> None:
+        try:
+            self.conn = pymysql.connect(
+            host= host,       
+            user= user,    
+            passwd= password,  
+            database= database
+            )
+        # database not initialized 
+        except pymysql.err.OperationalError:
+            self.conn = pymysql.connect(
+            host= host,       
+            user= user,    
+            passwd= password,  
+            )
+            
+            self.conn.cursor().execute("CREATE DATABASE %s"%(database))
+            self.conn.select_db(database)
+
+        #cursor = self.conn.cursor()
 
 
+    def close(self):
+        self.conn.close()
 
-try:
-    conn = pymysql.connect(
-    host= HOST,       
-    user= USER,    
-    passwd= PASSWORD,  
-    database= DATABASE
-    )
-# database not initialized 
-except pymysql.err.OperationalError:
-    conn = pymysql.connect(
-    host=HOST,      
-    user=USER,   
-    passwd=PASSWORD  
-    )
+    def drop_table(self,  table:str = 'musics'):
+        sql = "DROP TABLE IF EXISTS " + table  # 删除数据表 
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+
+    # {attribute1: property1, ...}
+    def create_table(self, attributes:dict, table:str = 'musics'):
+        sql = "CREATE TABLE IF NOT EXISTS musics (" + ''.join(
+            [ key + ' ' + attributes[key] + (',' if idx < (len(attributes) - 1) else '')
+             for idx, key in enumerate(attributes.keys())]) + ")"
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+        self.conn.commit()
+
+    # {attribute1: value1, ...}
+    def insert(self, new_row:dict , table:str = 'musics'):
+        sql = "INSERT INTO " + table + " (" \
+        + ''.join([ key +  (',' if idx < (len(new_row) - 1) else ') VALUES (' ) for idx, key in enumerate(new_row.keys())]) \
+        + ''.join(["%s" + (',' if idx < (len(new_row) - 1) else ')' ) for idx in range(len(new_row))]) 
+        val = [new_row[key] for key in new_row]
+        cursor = self.conn.cursor()
+        cursor.execute(sql, val)
+        self.conn.commit()  
+
+    # implement select sql and return a dictionary
+    def select(self, sql = None, table:str = 'musics')->dict:
+        if not sql:
+            sql = "SELECT * FROM " + table
+        cursor = self.conn.cursor()
+        cursor.execute(sql)   
+        res = cursor.fetchall()  
+
+        cursor.execute("DESC " + table)   
+        attr_record = cursor.fetchall()
+
+        data = [ { attr[0] :row[i] for i, attr in enumerate(attr_record) } for row in res]
+        return data
     
-    conn.cursor().execute("CREATE DATABASE %s"%(DATABASE))
-    conn.select_db(DATABASE)
+    # search by name 
+    def query_by_name(self, name:str, table:str = 'musics' ):
+        sql =  "SELECT * FROM " + table + " WHERE name LIKE '%%%s%%'"%(name)
+        # or 
+        sql =  "SELECT * FROM " + table + " WHERE name LIKE '%{}%'".format(name)
+        return self.select(sql = sql, table = table)
 
-cursor = conn.cursor()
-
-sql = "DROP TABLE IF EXISTS musics"  # 删除数据表 sites
- 
-cursor.execute(sql)
-
-
-sql = """CREATE TABLE musics (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name CHAR(40) NOT NULL,
-        time TIME NOT NULL,
-        author CHAR(40),
-        album CHAR(40)
-        )"""
-
-cursor.execute(sql)
+    # search by author 
+    def query_by_author(self, author:str, table:str = 'musics' ):
+        sql =  "SELECT * FROM " + table + " WHERE author LIKE '%%%s%%'"%(author)
+        # or 
+        sql =  "SELECT * FROM " + table + " WHERE author LIKE '%{}%'".format(author)
+        return self.select(sql = sql, table = table)    
 
 
-sql = "INSERT INTO musics (name, time, author, album) VALUES (%s, %s, %s ,%s)"
-val = ("yellow", ":04:11", "coldplay","")
-cursor.execute(sql,val)
-conn.commit()
+db = my_Database()
 
-cursor.execute("SELECT * FROM musics")
+db.drop_table()
+db.create_table(ATTRIBUTES)
 
-data = cursor.fetchall()
+row = {"name":"yellow", "time":":04:11", "author":"coldplay"}
+db.insert(row)
+
+data = db.query_by_author("old")
 print(data)
 
-conn.close()
-
-
-def insert(name, time, author, album):
-    sql = "INSERT INTO sites (name, time, author, album) VALUES (%s, %s, %s ,%s)"
-    val = (name, time, author,album)
+db.close()
