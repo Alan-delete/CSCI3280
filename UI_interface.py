5bbd2df5bf4217d0c69fe503bd8a31c6dbb17665
Alan-delete <62469498+Alan-delete@users.noreply.github.com>
2023-04-29 02:46:54 +0800

    OOP style

--- UI_interface.py
+++ UI_interface.py
@@ -2,7 +2,8 @@ import os
 import os.path as osp
 import time
 import tkinter
-import tkinter.filedialog
+import tkinter as tk
+from tkinter import filedialog
 import threading
 import pygame
 import random
@@ -14,39 +15,8 @@ USER = 'csci3280'
 PASSWORD = 'csci3280'
 DATABASE = 'project'
 
-root = tkinter.Tk()
-root.title('Music Player')
-root.geometry('460x600+500+100')
-root.resizable(False,False) 
 
-# tkinter variable object that shows music list
-List_var = None
-
-# folder to store the music 
-folder ='./'
-
-# res to store the list of music, in the dictionary form
-res = []
-
-# current selected music idx
-cur_idx = 0
-
-now_music = ''
-
-# Connect to the local database
-db = DataBase.mysql.my_Database(host = HOST,user= USER, password= PASSWORD, database= DATABASE)
-
-# get attributes information
-cursor = db.conn.cursor()
-cursor.execute("DESC music")   
-attr_record = cursor.fetchall()
-ATTRIBUTES = {attr[0] :attr[1] for attr in attr_record}
-# This is important to read the latest committed data
-cursor.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED")
-db.conn.commit()
-cursor.close()
-
-
+Attributes = None
 
 class MyDialog(tkinter.simpledialog.Dialog):
 
@@ -54,8 +24,9 @@ class MyDialog(tkinter.simpledialog.Dialog):
 
         self.entries = []
         self.result = {}
+        self.attributes =Attributes
         self.success = False
-        for idx, key in enumerate(ATTRIBUTES):
+        for idx, key in enumerate(self.attributes):
             tkinter.Label(master, text=key).grid(row=idx)
             e = tkinter.Entry(master)
             e.grid(row=idx, column=1)
@@ -80,324 +51,317 @@ class MyDialog(tkinter.simpledialog.Dialog):
             self.cancel()
 
     def apply(self):
-        for idx, key in enumerate(ATTRIBUTES):
+        for idx, key in enumerate(self.attributes):
             val = self.entries[idx].get()
             if (val != ""):
                 self.result[key] = val
         return 
 
 
-def buttonAddClick():
-    """
-    Add music 
-    :return:
-    """
 
-    # Get the files
-    files = tkinter.filedialog.askopenfilenames()
+class MusicPlayer:
+    def __init__(self, root):
+        self.root = root
+
+        # tkinter variable object that shows music list
+        self.List_var = tkinter.StringVar()
+        lb = tkinter.Listbox(root,listvariable=self.List_var)
+        lb.place(x=50,y=150,width=260,height=300)
+        lb.bind('<<ListboxSelect>>', self.List_on_select)
+
+        # folder to store the music 
+        self.folder ='./'
+
+        # res to store the list of music, in the dictionary form
+        self.res = []
+
+        # current selected music idx
+        self.cur_idx = 0
+
+
+        # Connect to the local database
+        self.db = DataBase.mysql.my_Database(host = HOST,user= USER, password= PASSWORD, database= DATABASE)
+
+        # get attributes information
+        cursor = self.db.conn.cursor()
+        cursor.execute("DESC music")   
+        attr_record = cursor.fetchall()
+        
+        self.ATTRIBUTES = {attr[0] :attr[1] for attr in attr_record}
+        global Attributes
+        Attributes = self.ATTRIBUTES
+        # This is important to read the latest committed data
+        cursor.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED")
+        self.db.conn.commit()
+        cursor.close()        
+       
+        # The callback of closing 
+        root.protocol('WM_DELETE_WINDOW', self.closeWindow)
+
+        # Add
+        buttonChoose = tkinter.Button(root,text='ADD',command=self.add_music)
+        
+        # Layout
+        buttonChoose.place(x=50,y=10,width=50,height=20)
+
+        # PLAY
+        self.pause_resume = tkinter.StringVar(root,value='PLAY')
+        buttonPlay = tkinter.Button(root,textvariable=self.pause_resume,command=self.play_music)
+        buttonPlay.place(x=190,y=10,width=50,height=20)
+        buttonPlay['state'] = 'normal'
+        
+
+        # END
+        buttonStop = tkinter.Button(root, text='STOP',command=self.stop_music)
+        buttonStop.place(x=120, y=10, width=50, height=20)
+        buttonStop['state'] = 'normal'
+
+        # NEXT
+        buttonNext = tkinter.Button(root, text='NEXT',command=self.switch_nxt)
+        buttonNext.place(x=260, y=10, width=50, height=20)
+        buttonNext['state'] = 'normal'
+        
+        # PREV
+        buttonPrev = tkinter.Button(root, text='PREV',command=self.switch_pre)
+        buttonPrev.place(x=330, y=10, width=50, height=20)
+        buttonPrev['state'] = 'normal'
+
+        # DELETE
+        buttonDelete = tkinter.Button(root, text='DELETE',command=self.delete_music)
+        buttonDelete.place(x=390, y=10, width=50, height=20)
+        buttonDelete['state'] = 'normal'
+
+
+        # Label
+        self.musicName = tkinter.StringVar(root, value='Current No music played')
+        labelName = tkinter.Label(root, textvariable=self.musicName)
+        labelName.place(x=10, y=30, width=260, height=20)
+
+        # Volume control
+        s = tkinter.Scale(root, label='Volume', from_=0, to=1, orient=tkinter.HORIZONTAL,
+                        length=240, showvalue=1, tickinterval=2, resolution=0.1,command=self.control_voice)
+        s.place(x=50, y=50, width=200)
+
+
+        show_list_thread = threading.Thread(target= self.fetch_show, daemon = True)
+        show_list_thread.start()
+
+        self.t = None 
+        root.mainloop()
+
     
-    # Get the names
-    musics = [music for music in files
-                if music.endswith(('.mp3','.wav','.ogg'))]
-
-    if len(musics) == 0:
-        return
-
-    for music in musics:
-        _, tail = osp.split(music)
-
-        row = {"name": tail, "time":"4:00"}
-        d = MyDialog(root)
-
-        if (d.success):
-            for key in d.result:
-                row[key] = d.result[key]
-            # create unique file name to store
-            row['location'] = osp.join(folder, str(random.randint(1,1e9)) +"_"+ row['name'] )
-            if (not row['location'].endswith(".wav") ): row['location'] += ".wav"
-            # add the file to the target location 
-            
-            if (db.insert_or_update(row)):
-                try:
-                    with open(row['location'], 'wb+') as tar_f:
-                        with open(music, "rb") as ori_f:
-                            tar_f.write(ori_f.read())
-                except:
-                    tkinter.messagebox.showerror(title=None, message="Fail to add file!")
-                    os.remove(row['location'])
-            else:
-                print("Please check if the information are correct!")
-                tkinter.messagebox.showerror(title=None, message="Please check if the information are correct!")
-            
-
-            
-
-def List_on_select(evt):
-    global cur_idx
-    w = evt.widget
-    index = int(w.curselection()[0])
-    print('You selected item %d' % (index))
-    cur_idx = index
-    buttonDelete['state'] = 'normal'
-
-# should be run on the thread
-def Fetch_Show():
-
-    global res
-    global List_var
-
-    global playing
-    playing = True
-    # Enable buttons
-    buttonPlay['state'] = 'normal'
-    buttonStop['state'] = 'normal'
-    # buttonPause['state'] = 'normal'
-    pause_resume.set('PLAY')
-
-    while (True): 
-        # should also gather information from other computers!
-        res = db.select()
-        ret = [i["name"] for i in res]
-
-        if (List_var):
-            List_var.set(ret)
-        else:
-            List_var = tkinter.StringVar()
-            List_var.set(ret)
-            lb = tkinter.Listbox(root,listvariable=List_var)
-            lb.place(x=50,y=150,width=260,height=300)
-            lb.bind('<<ListboxSelect>>', List_on_select)
-        time.sleep(1)
+    
+    def open_music(self):
+         self.music_file = filedialog.askopenfilename()
+         pygame.mixer.music.load(self.music_file)
+         # insert music file from database
+         music_title = self.music_file.split("/")[-1]
+         music_path = self.music_file.replace("'", "\\'")
+         insert_query = f"INSERT INTO music (title, path) VALUES ('{music_title}', '{music_path}');"
+        #  self.cursor.execute(insert_query)
+        #  self.cnx.commit()
 
 
-def play():
-    """
-    PLAY音乐
-    :return:
-    """ 
-    global cur_idx
-    if len(res):
-        pygame.mixer.init()
-        while playing:
+
+
+    def add_music(self):
+        # Get the files
+        files = tkinter.filedialog.askopenfilenames(title="Music files")
+        
+        # Get the names
+        musics = [music for music in files
+                    if music.endswith(('.mp3','.wav','.ogg'))]
+
+        if len(musics) == 0:
+            return
+
+        for music in musics:
+            head, tail = osp.split(music)
+            name = tail.replace(".wav", "")
+
+            row = {"name": name, "time":"4:00"}
+            d = MyDialog(self.root, self.ATTRIBUTES)
+
+            if (d.success):
+                for key in d.result:
+                    row[key] = d.result[key]
+                
+                # ask if there is lyrics file, if yes, store it with the same name + ".txt"
+                lyrics_file = tkinter.filedialog.askopenfile(title="Music lyrics")
+                if (lyrics_file):
+                    with open(osp.join(self.folder, row['name']+ ".txt" ), 'w+') as f:
+                        f.write(lyrics_file.read())
+
+                # create unique file name to store
+                row['location'] = osp.join(self.folder, str(random.randint(1,1e9)) +"_"+ row['name'] )
+                if (not row['location'].endswith(".wav") ): row['location'] += ".wav"
+                # add the file to the target location 
+                if (self.db.insert_or_update(row)):
+                    try:
+                        with open(row['location'], 'wb+') as tar_f:
+                            with open(music, "rb") as ori_f:
+                                tar_f.write(ori_f.read())
+                    except:
+                        tkinter.messagebox.showerror(title=None, message="Fail to add file!")
+                        os.remove(row['location'])
+                else:
+                    print("Please check if the information are correct!")
+                    tkinter.messagebox.showerror(title=None, message="Please check if the information are correct!")
+                
+
+                
+
+    def List_on_select(self, evt):
+        w = evt.widget
+        self.pause_resume.set('PLAY')
+        index = int(w.curselection()[0])
+        print('You selected item %d' % (index))
+        self.cur_idx = index
+
+
+    # should be run on the thread
+    def fetch_show(self):
+
+        self.playing = True
+
+        self.pause_resume.set('PLAY')
+
+        while (True): 
+            # should also gather information from other computers!
+            self.res = self.db.select()
+
+            ret = [i["name"] for i in self.res]
+            self.List_var.set(ret)
+            time.sleep(1)
+
+
+    def play(self):
+
+        if len(self.res):
+            pygame.mixer.init()
             if not pygame.mixer.music.get_busy():
-                netxMusic = res[cur_idx]['location']
+                netxMusic = self.res[self.cur_idx]['location']
                 if (netxMusic):
                     pygame.mixer.music.load(netxMusic.encode())
                     # PLAY
                     pygame.mixer.music.play(1)
                     #netxMusic = netxMusic.split('\\')[1:]
-                    musicName.set('playing......'+ res[cur_idx]['name'] )
+                    self.musicName.set('playing "'+ self.res[self.cur_idx]['name'] + '"')
                 else:
                     # here download and play music from other compurers 
-
                     print("music not found")
-                
-            else:
+                    return 
+
+            while self.playing:
                 time.sleep(0.1)
+            pygame.mixer.music.stop()
 
-# delete the current selected song
-def buttonDeleteClick():
-    global cur_idx
-    global res
-    global playing
-    playing = False
+    # delete the current selected song
+    def delete_music(self):
+        self.stop_music()
 
+        self.db.delete(id = self.res[self.cur_idx]['id'])
+        try:
+            os.remove(self.res[self.cur_idx]['location'])
+        except:
+            print("File already deleted")
+            
 
-    time.sleep(0.5)
-    try:
-        os.remove(res[cur_idx]['location'])
-        db.delete(id = res[cur_idx]['id'])
-    except:
-        print("Fail to delete")
+        if len(self.res) -1 == self.cur_idx:
+            self.cur_idx = 0
+        else:
+            self.cur_idx += 1
         
 
-    if len(res) -1 == cur_idx:
-        cur_idx = 0
-    else:
-        cur_idx = cur_idx + 1
-    
 
 
+    def play_music(self):
 
-def buttonPlayClick():
-    """
-    点击PLAY
-    :return:
-    """
-    global playing
-    buttonNext['state'] = 'normal'
+        if self.pause_resume.get() == 'PLAY':
+            self.pause_resume.set('PAUSE')
+            self.playing = True
+            self.t = threading.Thread(target=self.play)
+            self.t.start()
 
-    buttonPrev['state'] = 'normal'
+        elif self.pause_resume.get() == 'PAUSE':
+            # pygame.mixer.init()
+            pygame.mixer.music.pause()
 
-    if pause_resume.get() == 'PLAY':
-        pause_resume.set('STOP')
-        playing = True
+            self.pause_resume.set('CONTINUE')
 
-        t = threading.Thread(target=play)
-        t.start()
+        elif self.pause_resume.get() == 'CONTINUE':
+            # pygame.mixer.init()
+            pygame.mixer.music.unpause()
 
-    elif pause_resume.get() == 'STOP':
-        # pygame.mixer.init()
-        pygame.mixer.music.pause()
-
-        pause_resume.set('CONTINUE')
-
-    elif pause_resume.get() == 'CONTINUE':
-        # pygame.mixer.init()
-        pygame.mixer.music.unpause()
-
-        pause_resume.set('STOP')
+            self.pause_resume.set('PAUSE')
 
 
 
 
-def buttonStopClick():
-    """
-    STOPPLAY
-    :return:
-    """
-    global playing
-    playing = False
-    pygame.mixer.music.stop()
+    def stop_music(self):
+        self.playing = False
+        self.musicName.set("Current No music played")
+        self.pause_resume.set('PLAY')
+        if (self.t):
+            self.t.join()
+            self.t = None
 
 
-def buttonNextClick():
-    """
-    NEXT
-    :return:
-    """
-    global playing
-    playing = False
-    pygame.mixer.music.stop()
-    global cur_idx
-    if len(res) -1 == cur_idx:
-        cur_idx = 0
-    else:
-        cur_idx = cur_idx + 1
+    def switch_nxt(self):
 
-    playing = True
+        self.stop_music()
 
-    t = threading.Thread(target=play)
-    t.start()
+        if len(self.res) -1 == self.cur_idx:
+            self.cur_idx = 0
+        else:
+            self.cur_idx += 1
+
+        self.playing = True
+        self.pause_resume.set('PAUSE')
+        self.t = threading.Thread(target=self.play)
+        self.t.start()
 
 
-def closeWindow():
-    """
-    关闭窗口
-    :return:
-    """
+    def closeWindow(self):
+        self.stop_music()
 
-    global playing
+        try:
+            pygame.mixer.quit()
+        except:
+            pass
 
-    playing = False
-
-    time.sleep(0.3)
-
-    try:
-
-        pygame.mixer.music.stop()
-
-        pygame.mixer.quit()
-
-    except:
-
-        pass
-
-    root.destroy()
-    db.close()
+        self.root.destroy()
+        self.db.close()
 
 
-def control_voice(value=0.5):
-    """
-    :param value: 0.0-1.0
-    :return:
-    """
-    pygame.mixer.music.set_volume(float(value))
+    def control_voice(self,value=0.5):
+        pygame.mixer.music.set_volume(float(value))
 
 
-def buttonPrevClick():
-    """
-    PREV
-    :return:
-    """
-    global playing
+    def switch_pre(self):
 
-    playing = False
+        # stop the current song 
+        self.stop_music()
 
-    pygame.mixer.music.stop()
-    #
-    # pygame.mixer.quit()
-    global cur_idx
-    # cur_idx += 1
-    # cur_idx -= 1
-    if cur_idx == 0:
-        cur_idx = len(res) - 2
-        # cur_idx -= 1
-    elif cur_idx == len(res) - 1:
-        cur_idx -= 2
-    else:
-        cur_idx -= 2
-        # cur_idx -= 1
+        if self.cur_idx == 0:
+            self.cur_idx = len(self.res) - 2
+
+        else:
+            self.cur_idx -= 1
 
 
-    playing = True
-    t = threading.Thread(target=play)
-    t.start()
-
+        self.playing = True
+        self.pause_resume.set('PAUSE')
+        self.t = threading.Thread(target=self.play)
+        self.t.start()
+        
 if __name__ == "__main__":
+    #root = tk.Tk()
+    #root.geometry("400x300")  # resize the window
+    root = tkinter.Tk()
+    root.title('Music Player')
+    root.geometry('460x600+500+100')
+    root.resizable(False,False) 
 
-    # The callback of closing 
-    root.protocol('WM_DELETE_WINDOW', closeWindow)
-
-    # Add
-    buttonChoose = tkinter.Button(root,text='ADD',command=buttonAddClick)
-    
-    # Layout
-    buttonChoose.place(x=50,y=10,width=50,height=20)
-
-    # PLAY
-    pause_resume = tkinter.StringVar(root,value='PLAY')
-    buttonPlay = tkinter.Button(root,textvariable=pause_resume,command=buttonPlayClick)
-    buttonPlay.place(x=190,y=10,width=50,height=20)
-    buttonPlay['state'] = 'disabled'
-
-
-    # END
-    buttonStop = tkinter.Button(root, text='STOP',command=buttonStopClick)
-    buttonStop.place(x=120, y=10, width=50, height=20)
-    buttonStop['state'] = 'disabled'
-
-    # NEXT
-    buttonNext = tkinter.Button(root, text='NEXT',command=buttonNextClick)
-    buttonNext.place(x=260, y=10, width=50, height=20)
-    buttonNext['state'] = 'disabled'
-    
-    # PREV
-    buttonPrev = tkinter.Button(root, text='PREV',command=buttonPrevClick)
-    buttonPrev.place(x=330, y=10, width=50, height=20)
-    buttonPrev['state'] = 'disabled'
-
-    # DELETE
-    buttonDelete = tkinter.Button(root, text='DELETE',command=buttonDeleteClick)
-    buttonDelete.place(x=390, y=10, width=50, height=20)
-    buttonDelete['state'] = 'disabled'
-
-
-    # Label
-    musicName = tkinter.StringVar(root, value='Current No music played')
-    labelName = tkinter.Label(root, textvariable=musicName)
-    labelName.place(x=10, y=30, width=260, height=20)
-
-    # Volume control
-    s = tkinter.Scale(root, label='Volume', from_=0, to=1, orient=tkinter.HORIZONTAL,
-                    length=240, showvalue=1, tickinterval=2, resolution=0.1,command=control_voice)
-    s.place(x=50, y=50, width=200)
-
-
-    show_list_thread = threading.Thread(target= Fetch_Show, daemon = True)
-    show_list_thread.start()
-
-    # 显示
-    root.mainloop()
-
+    app = MusicPlayer(root)
 
--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import socket
 
+# DESKTOP-MHI4JFV
 HOST = socket.gethostname()
+HOSTIP = socket.gethostbyname(HOST)
+print(HOSTIP)
+print(socket.gethostbyname("localhost"))
 PORT = 1600
 
 def client_part():
@@ -10,15 +14,84 @@ def client_part():
     s.close()
 
 
-if __name__ == '__main__':
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    # here 10 is the maximun number of clients  
-    server.listen(10)
-    print("start server")
+# if __name__ == '__main__':
+#     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server.bind((HOST, PORT))
+#     # here 10 is the maximun number of clients  
+#     server.listen(10)
+#     print("start server")
 
-    while True:
-        client, addr = server.accept()
-        print( "connect to address:", addr)
-        client.send('test'.encode())
-        client.close()
+#     while True:
+#         client, addr = server.accept()
+#         print( "connect to address:", addr)
+#         client.send('test'.encode())
+#         client.close()
+
+# new method using pythonp2p
+from pythonp2p import Node
+
+class Mynode(Node):
+  def on_message(message, sender, private):
+    # Gets called everytime there is a new message
+    pass
+  
+def test():
+
+    fhash = ""
+    #Empty string means '0.0.0.0' which means that program will bind to and receive requests from all NICs
+    node0 = Node("", 65434)
+    node1 = Node("", 65435)
+
+    node0.start()
+    node1.start()
+    print(node0.ip)
+    print(node1.ip)
+
+    #node0.connect_to(HOST, 65435)
+
+
+    def test_node_connect():
+        assert len(node0.nodes_connected) == 1
+        assert len(node1.nodes_connected) == 1
+
+
+    def test_node_message():
+        node0.send_message("node test")
+        assert len(node1.msgs.keys()) == 1
+        assert len(node0.msgs.keys()) == 1
+
+
+    def test_node_private_message():
+        node0.send_message("TEST MESSAGE", reciever=node1.id)
+
+
+    def test_files_add():
+        global fhash
+        fhash = node1.addfile("LICENSE")
+
+
+    def test_file_request():
+        node0.requestFile(fhash)
+        print(node0.file_manager.getallfiles())
+        assert len(node0.requested) == 1
+        assert len(node1.msgs.keys()) == 2
+
+    #test_node_connect()
+    node0.stop()
+    node1.stop()
+
+
+if __name__ == "__main__":
+    test()
+    # node = Mynode(HOST, PORT)  # start the node
+    # node.start()
+    # print(node.id)
+    # ip = HOST
+    # port = PORT
+    # node.connect_to(ip, port)
+    # print(node.node_connected)
+    # data = {}
+    # try:
+    #     node.send_message(data, receiver=None)
+    # except:
+    #     print("Wrong")
