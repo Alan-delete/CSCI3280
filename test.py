ef591d65d9016318edf38e68855768605dc0ea9d
Zjiangc2000 <75105169+Zjiangc2000@users.noreply.github.com>
2023-05-02 15:05:33 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+from audio_decoder import AudioDecoder
+
+def main():
+    file_path = input("Input the file name of your audio file (WAV or MP3): ")
+    decoder = AudioDecoder(file_path)
+    audio_array, num_channels, sample_width, sample_rate = decoder.decode()
+    decoder.play_audio(audio_array, num_channels, sample_width, sample_rate)
+
+if __name__ == "__main__":
+    main()
